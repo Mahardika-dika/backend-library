d20e4158b763d6cd9c8d21d60d791d091fba33f6
Marshal-bd <arfurm198@gmail.com>
2026-02-06 18:15:25 +0700

    Version: 2.7

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ extended: true }));
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,13 +21,13 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('JobSeeker')
     .setDescription('BackEnd System for JobSeeker Application')
-    .setVersion('2.3')
+    .setVersion('2.6')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('libraryApp', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
