40abcae9f4b3f5a573db0cbed4c0a190796d28c9
Marshal-bd <arfurm198@gmail.com>
2026-02-10 14:04:57 +0700

    Version: 2.7

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
@@ -7,6 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './guard/role.guard';
 
+@Global()
 @Module({
   imports: [
     JwtModule.register({
